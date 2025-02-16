930637eb360252064c258d391edc53cfd2d878e6
Roberto Erives <robertoerives@outlook.com>
2025-02-16 14:16:09 -0600

    se agrego la parte de crear lista

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
 let nombresSorteados = [];
 
 
-
-
-
-
-
-
-
 function agregarAmigo(text) {
     let nombreEscrito = document.getElementById('amigo').value;
-  
 
     if (nombreEscrito === ''){
-   alert('Porfavor, inserte un nombre.')}
-    else {
-    nombresSorteados.push(nombreEscrito);
-    console.log(nombresSorteados);
-    limpiarCaja('amigo');
+    alert('Porfavor, inserte un nombre.')}
+      else {
+      nombresSorteados.push(nombreEscrito);
+      limpiarCaja('amigo');
+      
+      console.log(nombresSorteados);
+      crearLista();
     } 
 }
 
 function limpiarCaja(id) {
     let textoCaja =document.getElementById(id);
-    textoCaja.value = '';
+    textoCaja.value = '';}
+
+function crearLista () {
+  let lista = document.getElementById('listaAmigos');
+  lista.innerHTML = '';
+nombresSorteados.forEach(nombre => {
+  let li = document.createElement('li');
+  li.textContent = nombre;
+  lista.appendChild(li);
+
+});
 }
+
+ 
+
+    
