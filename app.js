a1c214768396ddcc237a77b93d749b09abe7d782
Roberto Erives <robertoerives@outlook.com>
2025-02-16 17:11:32 -0600

    amigo Sorteado y generar numero aleatoreo.

--- app.js
+++ app.js
@@ -11,34 +11,49 @@ function agregarAmigo(text) {
     alert('Porfavor, inserte un nombre.')}
       else {
       nombresSorteados.push(nombreEscrito);
-      limpiarCaja('amigo');
+      limpiarCaja('amigo','');
+      limpiarMensaje('resultao', '');
       
       console.log(nombresSorteados);
       crearLista();
     } 
-}
+  }
 
-function limpiarCaja(id) {
+function limpiarMensaje(id, text) {
     let textoCaja =document.getElementById(id);
-    textoCaja.value = '';}
+    textoCaja.textContent = text;}
+
+
+function limpiarCaja(id, text) {
+    let textoCaja =document.getElementById(id);
+    textoCaja.value = text;}
 
 function crearLista () {
-  let lista = document.getElementById('listaAmigos');
-  lista.innerHTML = '';
-nombresSorteados.forEach(nombre => {
-  let li = document.createElement('li');
-  li.textContent = nombre;
-  lista.appendChild(li);
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    nombresSorteados.forEach(nombre => {
+    let li = document.createElement('li');
+    li.textContent = nombre;
+    lista.appendChild(li);
 
-});
-}
+    });
+  }
 
-function sortearAmigo() {
+function resultadoFinal(){
+    let resultado = document.getElementById('resultao');
+    sorteoIndice = Math.floor(Math.random()*nombresSorteados.length);
+    let amigoSorteado = nombresSorteados[sorteoIndice];
 
-sorteoIndice = Math.floor(Math.random()*nombresSorteados.length);
-let amigoSorteado = nombresSorteados[sorteoIndice];
-let resultado = document.getElementById('resultao');
-resultado.textContent = `Tu amigo Secreto es ${amigoSorteado}.`
+    resultado.textContent = `Tu amigo Secreto es ${amigoSorteado}.`
+  }
+ 
+function sortearAmigo() { 
+    let resultado =document.getElementById('resultao');
 
-}
-
+    if (nombresSorteados.length === 0){
+    resultado.textContent = 'Falta agregar nombres';
+    }
+    else {
+    resultadoFinal();
+    } 
+  }
